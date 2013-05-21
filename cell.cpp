cc7f1ff243a5a18b99a2930fa0bf80558eb9fbf7
Steven Vergenz <steve.vergenz.ctr@adlnet.gov>
2013-05-21 13:13:26 -0400

    Fleshed out cell a little more

--- cell.cpp
+++ cell.cpp
@@ -1,7 +1,33 @@
 #include "cell.h"
 
-Cell::Cell(QSet<Cell&>* row, QSet<Cell&>* column, QSet<Cell&>* block, char value = Cell::UNKNOWN)
+Cell::Cell(QSet<Cell&>& row, QSet<Cell&>& column, QSet<Cell&>& block, char value = Cell::UNKNOWN)
 : row(row), column(column), block(block), value(value)
 {
+	if( value != UNKNOWN ){
+		given = true;
+	}
+}
 
+int Cell::domainSize(){
+	return domain.size();
+}
+
+bool Cell::inDomain(char value){
+	return domain.contains(value);
+}
+
+bool Cell::dependentCells(){
+	return row+column+block;
+}
+
+bool Cell::setValue(char value)
+{
+	if( domain.contains(value) )
+	{
+		this->value = value;
+		domain.clear();
+		return true;
+	}
+	else
+		return false;
 }
--- cell.h
+++ cell.h
@@ -6,15 +6,29 @@
 class Cell
 {
 public:
+
+	// represents an unknown value for the cell
 	static const char UNKNOWN = -1;
 
-	Cell(QSet<Cell&>* row, QSet<Cell&>* column, QSet<Cell&>* block, char value);
+	// constructor, fixes position and value of cell
+	Cell(QSet<Cell &> &row, QSet<Cell &> &column, QSet<Cell &> &block, char value);
 
+	// returns the number of potential values in the cell
+	int domainSize();
+
+	// returns whether a given value is in the domain
+	bool inDomain(char value);
+
+	// returns a set of cells dependent on this one
+	bool dependentCells();
+
+	// attempts to set value of cell, returns false if invalid
+	bool setValue(char value);
 
 private:
-	QSet<Cell&>* row;
-	QSet<Cell&>* column;
-	QSet<Cell&>* block;
+	QSet<Cell&>& row;
+	QSet<Cell&>& column;
+	QSet<Cell&>& block;
 
 	char value;
 	bool given;
