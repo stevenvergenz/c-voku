12825fcd660af88af8a9e1278d04acd7546deaa0
Steven Vergenz <steve.vergenz.ctr@adlnet.gov>
2013-05-23 14:42:01 -0400

    Cleaned up custom grid

--- metatableitemdelegate.cpp
+++ metatableitemdelegate.cpp
@@ -3,7 +3,9 @@
 MetatableItemDelegate::MetatableItemDelegate(QObject *parent)
  : QStyledItemDelegate(parent)
 {
-	heavyPen.setWidth(2);
+	heavyPen.setWidth(1);
+	heavyPen.setColor( QColor("black") );
+
 	lightPen.setWidth(1);
 	lightPen.setColor( QColor("lightgrey") );
 }
@@ -26,6 +28,16 @@ void MetatableItemDelegate::paint(QPainter *painter, const QStyleOptionViewItem 
 		painter->drawLine(option.rect.topLeft(), option.rect.topRight());
 	}
 
+	/*// paint bottom gridline
+	if( index.row() % blockSize == blockSize-1 && index.row() != gridSize-1 ){
+		painter->setPen(heavyPen);
+		painter->drawLine(option.rect.bottomLeft(), option.rect.bottomRight());
+	}
+	else {
+		painter->setPen(lightPen);
+		painter->drawLine(option.rect.bottomLeft(), option.rect.bottomRight());
+	}*/
+
 	// paint left gridline
 	if( index.column() % blockSize == 0 && index.column() != 0 ){
 		painter->setPen(heavyPen);
@@ -36,6 +48,15 @@ void MetatableItemDelegate::paint(QPainter *painter, const QStyleOptionViewItem 
 		painter->drawLine(option.rect.topLeft(), option.rect.bottomLeft());
 	}
 
+	/*// paint right gridline
+	if( index.column() % blockSize == blockSize-1 && index.column() != gridSize-1 ){
+		painter->setPen(heavyPen);
+		painter->drawLine(option.rect.topRight(), option.rect.bottomRight());
+	}
+	else {
+		painter->setPen(lightPen);
+		painter->drawLine(option.rect.topRight(), option.rect.bottomRight());
+	}*/
 
 	painter->restore();
 	this->QStyledItemDelegate::paint(painter, option, index);
